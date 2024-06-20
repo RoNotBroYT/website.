14594fc6b382fab82b434f6187b4bd5ce83cfdff
RoNotBroYT <ronotbro@gmail.com>
2024-06-20 20:15:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // You can add further logic here, e.g., sending form data to a server
+  alert('Form submitted!');
+});
